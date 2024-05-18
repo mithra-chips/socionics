fc693db0c37dceec70735193220837646747786e
Mithra <chyouko_zhang@hotmail.com>
2024-05-18 20:47:20 +0900

    直接部署在github pages时，使用哈希模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const deleteChatComponent = () => import('@/pages/chat/chat-messages-delete.vue')
 const chatPageComponent = () => import('@/pages/chat/chat-page.vue')
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // 使用哈希模式 (hash mode) 而不是HTML5历史模式 (history mode)
+  // https://mithra-chips.github.io/socionics/#
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: chatPageComponent },
     { path: '/delete-chat', component: deleteChatComponent },
