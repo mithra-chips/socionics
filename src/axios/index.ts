5d4bed1f5b792194fb3dbc7fd52fa6cb848ab4ee
Mithra <chyouko_zhang@hotmail.com>
2024-05-19 20:26:27 +0900

    Changes to be committed: 	new file:   dist/assets/chat-messages-delete-Dhe8yHeA.js 	new file:   dist/assets/chat-page-B4_xchS-.js 	new file:   dist/assets/chat-page-BGq0zbLo.css 	new file:   dist/assets/index-Cvc5VgZ3.js 	new file:   dist/assets/index-RlxYn1Ao.css 	new file:   dist/favicon.ico 	new file:   dist/index.html

--- src/axios/index.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const BASE_URL = 'https://chat-server-r7lx.onrender.com/'
-axios.defaults.baseURL = BASE_URL
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
-axios.defaults.headers.common['Content-Type'] = 'application/json'
-export default axios
